1371a3b072e58371ba6951f490c001e012f3850f
Chen Pan <happychinapc@gmail.com>
2022-04-02 17:56:53 +0800

    Projects.kt

---
+++ agp/src/main/java/me/bytebeats/agp/gav/switcher/Projects.kt
@@ -0,0 +1,36 @@
+package me.bytebeats.agp.gav.switcher
+
+import org.gradle.api.Project
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created at 2022/4/2 17:52
+ * @Version 1.0
+ * @Description TO-DO
+ */
+private const val GAV_SWITCH_ENABLED = "gavToModule.enable"
+private const val GAV_SWITCH_SKIPS = "gavToModule.skips"
+
+fun Project.isEnabled(): Boolean {
+    try {
+        if (this.rootProject.hasProperty(GAV_SWITCH_ENABLED)) {
+            return this.rootProject.properties[GAV_SWITCH_ENABLED].toString().toBoolean()
+        }
+    } catch (ignore: Exception) {
+
+    }
+    return true
+}
+
+fun Project.skips(): List<String> {
+    try {
+        if (this.rootProject.hasProperty(GAV_SWITCH_SKIPS)) {
+            return this.rootProject.properties[GAV_SWITCH_SKIPS].toString().split(",")
+        }
+    } catch (ignore: Exception) {
+
+    }
+    return emptyList()
+}
